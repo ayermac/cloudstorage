e845a996eb828a2719d1abcc8efcb72fca9dd366
Jason <codemac@163.com>
2017-09-25 21:04:20 +0800

    更新 Sql

--- sql/cloud_storage.sql
+++ sql/cloud_storage.sql
@@ -10,10 +10,24 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-09-22 21:08:03
+Date: 2017-09-25 21:03:11
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_name` (`username`),
+  KEY `user_id` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for user_setting
@@ -28,6 +42,7 @@ CREATE TABLE `user_setting` (
   `region` varchar(2) NOT NULL COMMENT '区域',
   `timeout` smallint(3) unsigned NOT NULL DEFAULT '60' COMMENT '超时',
   `cdn` varchar(2) DEFAULT NULL COMMENT '是否开启cdn',
+  `user_id` int(10) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `app_id` (`app_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
